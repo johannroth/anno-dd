eb52ef7f869d57147ff39c1f7f7b4c960b567e7c
Johann Roth <johann.roth@esveo.com>
2025-02-08 20:43:49 +0100

    fixes font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${montserrat.variable} ${inter.variable} bg-gray-50 font-montserrat) tracking-tight text-gray-900 antialiased`}
+        className={`${inter.variable} ${montserrat.variable} bg-gray-50 font-montserrat tracking-tight text-gray-900 antialiased`}
       >
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
           <MainLayout>{children}</MainLayout>
